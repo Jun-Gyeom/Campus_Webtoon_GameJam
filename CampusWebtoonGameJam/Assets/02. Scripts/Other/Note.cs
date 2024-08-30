0dd254acc90346bc74c3814ceb5bec70f90aa39a
Jun-Gyeom <Jun-Gyeom@users.noreply.github.com>
2024-08-31 02:04:33 +0900

    메뉴(메인) 씬 작업

--- CampusWebtoonGameJam/Assets/02. Scripts/Other/Note.cs
+++ CampusWebtoonGameJam/Assets/02. Scripts/Other/Note.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Note : MonoBehaviour
 {
     public float noteSpeed;
+    public NoteType type;
     private void FixedUpdate()
     {
         transform.position += Vector3.left * noteSpeed * Time.fixedDeltaTime;
